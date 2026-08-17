dac98804aace4c3d9eead5ef6bcdbba2f11af8cb
TV <123402150+TunanshV@users.noreply.github.com>
2026-08-17 10:11:38 +0000

    Completed till 2:35:00

---
+++ First.java
@@ -0,0 +1,73 @@
+class First{
+    public static void main(String args[]){
+        // int a;
+        // int b;
+        // System.out.println("Hello World");
+
+        int num=9;
+        byte by=127;
+        short sh= 558;
+        long l= 5854l;
+
+        float f= 5.8f;
+        double d= 5.8;
+
+        char ch= 'k';
+
+        boolean b= true;
+
+
+        //literals
+        int numm= 0b101;
+        System.out.println(numm);
+        // we can use _ in between numbers
+
+
+        // conditionals-->>> if-else
+
+        //  ternary operator -->   condition?if-true:if-false
+
+        // int day=1;
+
+        // switch(day){
+        //       case 1:
+        //         System.out.println("Monday");
+        //         break;
+        //       case 1:
+        //         System.out.println("Monday");
+        //         break;
+        //       case 2:
+        //         System.out.println("Tuesday");
+        //         break;
+        //       case 3:
+        //         System.out.println("Wednesday");
+        //         break;
+        //       case 4:
+        //         System.out.println("Thursday");
+        //         break;
+        //       case 5:
+        //         System.out.println("Friday");
+        //         break;
+        //       case 6:
+        //         System.out.println("Saturday");
+        //         break;
+        //       case 7:
+        //         System.out.println("Sunday");
+        //         break;
+        //       default:
+        //          System.out.println("Enter a valid number");
+        // }
+
+
+        // loops->while,do-while,for
+
+        int i=5;
+
+        do 
+        {
+            System.out.println("Hi"+i);
+        }while(i<=4);
+
+
+    }
+}
